e2f9f17412e56885445bf2193cf6de3740a443c4
Emma Waffle <emma@waffle.coffee>
2021-06-15 22:24:55 -0400

    It builds, it runs, it doesn't work

--- src/main/java/dev/frydae/emcutils/mixins/ConnectScreenMixin.java
+++ src/main/java/dev/frydae/emcutils/mixins/ConnectScreenMixin.java
@@ -1,7 +1,9 @@
 package dev.frydae.emcutils.mixins;
 
 import dev.frydae.emcutils.utils.Util;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ConnectScreen;
+import net.minecraft.client.network.ServerAddress;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,9 +11,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ConnectScreen.class)
 public class ConnectScreenMixin {
-  @Inject(method = "connect", at = @At(value = "HEAD"))
-  public void onConnect(String address, int port, CallbackInfo ci) {
-    Util.setServerAddress(address);
-    Util.IS_ON_EMC = address.matches(".*.emc.gs?.");
+  @Inject(method = "connect(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/network/ServerAddress;)V", at = @At(value = "HEAD"))
+  public void onConnect(MinecraftClient client, ServerAddress address, CallbackInfo ci) {
+    Util.setServerAddress(String.valueOf(address));
+    Util.IS_ON_EMC = address.equals(".*.emc.gs?.");
   }
 }
