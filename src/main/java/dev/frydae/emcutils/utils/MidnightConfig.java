e8dd5cc14fadc71d2cf1cb23d6d6e9c2f40d8153
Emma Waffle <emmaffle@modrinth.com>
2021-08-22 17:19:50 -0400

    fix a copyright header

--- src/main/java/dev/frydae/emcutils/utils/MidnightConfig.java
+++ src/main/java/dev/frydae/emcutils/utils/MidnightConfig.java
@@ -1,7 +1,5 @@
 /*
- * Copyright (c) 2021 MrFrydae
- * Copyright (c) 2021 wafflecoffee
- * Copyright (c) 2021 djlawler
+ * Copyright (c) 2020 TeamMidnightDust
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
