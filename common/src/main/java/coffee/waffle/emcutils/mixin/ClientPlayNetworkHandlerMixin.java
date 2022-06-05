de5f17fc0aef7ea5be93cf926a92ee87ac7e8a7f
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-06-05 18:41:09 -0400

    Update to 1.19-rc2

--- common/src/main/java/coffee/waffle/emcutils/mixin/ClientPlayNetworkHandlerMixin.java
+++ common/src/main/java/coffee/waffle/emcutils/mixin/ClientPlayNetworkHandlerMixin.java
@@ -8,35 +8,34 @@ import coffee.waffle.emcutils.util.Util;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.network.MessageType;
+import net.minecraft.network.packet.s2c.play.ChatMessageS2CPacket;
 import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
 import net.minecraft.network.packet.s2c.play.OpenScreenS2CPacket;
 import net.minecraft.screen.ScreenHandlerType;
-import net.minecraft.unmapped.C_tzcijmwg;
-import net.minecraft.unmapped.C_zzdolisx;
 import net.minecraft.util.ActionResult;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class ClientPlayNetworkHandlerMixin {
-  private static final ClientPlayerEntity player = MinecraftClient.getInstance().player;
+  @Shadow @Final private MinecraftClient client;
 
-  @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;method_43592(Lnet/minecraft/network/MessageType;Lnet/minecraft/text/Text;Lnet/minecraft/unmapped/C_tzcijmwg;)V"), method = "method_44073", cancellable = true)
-  void emcutils$onPreReceiveMessage(MessageType messageType, C_zzdolisx c_zzdolisx, C_tzcijmwg c_tzcijmwg, CallbackInfo info) {
-    ActionResult result = ChatCallback.PRE_RECEIVE_MESSAGE.invoker().onPreReceiveMessage(player, c_zzdolisx.unsignedContent().get());
+  @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;handleChatMessage(Lnet/minecraft/network/MessageType;Lnet/minecraft/unmapped/C_zzdolisx;Lnet/minecraft/unmapped/C_tzcijmwg;)V"), method = "onChatMessage", cancellable = true)
+  void emcutils$onPreReceiveMessage(ChatMessageS2CPacket packet, CallbackInfo info) {
+    ActionResult result = ChatCallback.PRE_RECEIVE_MESSAGE.invoker().onPreReceiveMessage(client.player, packet.method_43885().signedContent());
 
     if (result != ActionResult.PASS) {
       info.cancel();
     }
   }
 
-  @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;method_43592(Lnet/minecraft/network/MessageType;Lnet/minecraft/text/Text;Lnet/minecraft/unmapped/C_tzcijmwg;)V", shift = At.Shift.AFTER), method = "method_44073")
-  void emcutils$onPostReceiveMessage(MessageType messageType, C_zzdolisx c_zzdolisx, C_tzcijmwg c_tzcijmwg, CallbackInfo info) {
-    ChatCallback.POST_RECEIVE_MESSAGE.invoker().onPostReceiveMessage(player, c_zzdolisx.unsignedContent().get());
+  @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;handleChatMessage(Lnet/minecraft/network/MessageType;Lnet/minecraft/unmapped/C_zzdolisx;Lnet/minecraft/unmapped/C_tzcijmwg;)V", shift = At.Shift.AFTER), method = "onChatMessage")
+  void emcutils$onPostReceiveMessage(ChatMessageS2CPacket packet, CallbackInfo info) {
+    ChatCallback.POST_RECEIVE_MESSAGE.invoker().onPostReceiveMessage(client.player, packet.method_43885().signedContent());
   }
 
   @Inject(at = @At("TAIL"), method = "onGameJoin")
