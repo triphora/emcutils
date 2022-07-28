f54c85e56e0b1540c3ac2a390829fcc2e1de5865
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-07-28 14:56:31 -0400

    Disable DFU optimizations b/c DFU is annoying

---
+++ common/src/main/java/coffee/waffle/emcutils/mixin/MainMixin.java
@@ -0,0 +1,14 @@
+package coffee.waffle.emcutils.mixin;
+
+import net.minecraft.client.main.Main;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(Main.class)
+public class MainMixin {
+  @ModifyArg(method = "main([Ljava/lang/String;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/main/Main;main([Ljava/lang/String;Z)V"), index = 1, require = 0)
+  static boolean emcutils$disableDfuOptimizations(boolean optimizeDataFixer) {
+    return false;
+  }
+}
