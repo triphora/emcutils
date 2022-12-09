5fa2308e21917e910b81c198e85c2755a21f9ae1
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-12-08 20:56:25 -0500

    Update forge and fix home command

--- common/src/main/java/coffee/waffle/emcutils/listener/CommandListener.java
+++ common/src/main/java/coffee/waffle/emcutils/listener/CommandListener.java
@@ -18,66 +18,58 @@ public class CommandListener {
 	}
 
 	private static ActionResult handleResidenceHomeCommand(ClientPlayerEntity player, String command, List<String> args) {
-		if (command.equalsIgnoreCase("home")) {
-			int num = 1;
-			String loc = "";
+		if (!command.equalsIgnoreCase("home")) {
+			return ActionResult.PASS;
+		}
 
-			if (args.size() != 0) {
-				if (args.size() == 1) {
-					if (NumberUtils.isParsable(args.get(0))) {
-						num = Integer.parseInt(args.get(0));
-					} else {
-						loc = args.get(0);
-					}
-				} else if (args.size() == 2) {
-					if (NumberUtils.isParsable(args.get(0))) {
-						num = Integer.parseInt(args.get(0));
-						loc = args.get(1);
-					}
-				}
-			} else return ActionResult.PASS;
+		int num = 1;
+		String loc = "";
 
-			String resName = MinecraftClient.getInstance().player.getEntityName() + (num > 1 ? "-" + num : ""); // FIXME: this is what causes #33
-
-			EmpireServer server = VisitResidenceHandler.getResidenceServer(resName);
-
-			if (server != EmpireServer.NULL && server != Util.getCurrentServer()) {
-				Util.onJoinCommandQueue.add("v " + resName + " " + loc);
-
-				server.sendToServer();
-
-				return ActionResult.FAIL;
+		if (args.size() == 1) {
+			if (NumberUtils.isParsable(args.get(0))) {
+				num = Integer.parseInt(args.get(0));
+			} else {
+				loc = args.get(0);
+			}
+		} else if (args.size() == 2) {
+			if (NumberUtils.isParsable(args.get(0))) {
+				num = Integer.parseInt(args.get(0));
+				loc = args.get(1);
 			}
 		}
 
-		return ActionResult.PASS;
+		String resName = MinecraftClient.getInstance().player.getEntityName() + (num > 1 ? "-" + num : "");
+
+		return handleResCommandsCommon(resName, loc);
 	}
 
 	private static ActionResult handleResidenceVisitCommand(ClientPlayerEntity player, String command, List<String> args) {
-		if (command.equalsIgnoreCase("v") || command.equalsIgnoreCase("visit")) {
-			if (args.isEmpty()) {
-				return ActionResult.PASS;
-			}
+		if (!(command.equalsIgnoreCase("v") || command.equalsIgnoreCase("visit")) || args.isEmpty()) {
+			return ActionResult.PASS;
+		}
 
-			String res = args.get(0);
-			String loc = args.size() > 1 ? args.get(1) : "";
+		String res = args.get(0);
+		String loc = args.size() > 1 ? args.get(1) : "";
 
-			if (res.contains("@")) {
-				String[] split = res.split("@");
+		if (res.contains("@")) {
+			String[] split = res.split("@");
 
-				res = split[0];
-				loc = (split.length > 1 ? split[1] : "");
-			}
+			res = split[0];
+			loc = (split.length > 1 ? split[1] : "");
+		}
 
-			EmpireServer server = VisitResidenceHandler.getResidenceServer(res);
+		return handleResCommandsCommon(res, loc);
+	}
 
-			if (server != EmpireServer.NULL && server != Util.getCurrentServer()) {
-				Util.onJoinCommandQueue.add("v " + res + " " + loc);
+	private static ActionResult handleResCommandsCommon(String res, String loc) {
+		EmpireServer server = VisitResidenceHandler.getResidenceServer(res);
 
-				server.sendToServer();
+		if (server != EmpireServer.NULL && server != Util.getCurrentServer()) {
+			Util.onJoinCommandQueue.add("v " + res + " " + loc);
 
-				return ActionResult.FAIL;
-			}
+			server.sendToServer();
+
+			return ActionResult.FAIL;
 		}
 
 		return ActionResult.PASS;
