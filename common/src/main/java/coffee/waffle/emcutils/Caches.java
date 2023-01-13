53038d23cc15381f15cd06ac7278c5db5720cb1b
MrFrydae <MrFrydae@gmail.com>
2023-01-12 21:36:18 -0500

    Fixes

--- common/src/main/java/coffee/waffle/emcutils/Caches.java
+++ common/src/main/java/coffee/waffle/emcutils/Caches.java
@@ -1,45 +1,24 @@
 package coffee.waffle.emcutils;
 
+import coffee.waffle.emcutils.feature.Nameplates;
 import com.google.common.cache.*;
-import com.google.common.collect.Lists;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public final class Caches {
-	private Caches() {}
 
-	private static final CacheLoader<PlayerEntity, Text> NAMEPLATE_CACHE_LOADER = new CacheLoader<PlayerEntity, Text>() {
+	private static final CacheLoader<PlayerEntity, Text> NAMEPLATE_CACHE_LOADER = new CacheLoader<>() {
 		@Override
-		public Text load(@NotNull PlayerEntity player) throws Exception {
-			Collection<PlayerListEntry> playerList = Objects.requireNonNull(MinecraftClient.getInstance().getNetworkHandler()).getPlayerList();
-
-			PlayerListEntry entry = null;
-			for (PlayerListEntry playerListEntry : playerList) {
-				List<Text> siblings = playerListEntry.getDisplayName().getSiblings();
-				if (siblings.size() > 1 && siblings.get(1).contains(player.getName())) {
-					entry = playerListEntry;
-				}
-			}
+		@NotNull
+		public Text load(@NotNull PlayerEntity player) {
+			PlayerListEntry entry = Nameplates.findPlayerListEntry(player);
 
 			if (entry != null) {
-				List<Text> siblings = Lists.newArrayList(entry.getDisplayName().getSiblings());
-				siblings.remove(0); // Remove Server Tag
-				MutableText text = Text.empty();
-
-				for (Text sibling : siblings) {
-					text.append(sibling);
-				}
-
-				return text;
+				return Nameplates.parseDisplayName(entry);
 			}
 
 			return player.getName();
--- common/src/main/java/coffee/waffle/emcutils/mixin/PlayerEntityMixin.java
+++ common/src/main/java/coffee/waffle/emcutils/mixin/PlayerEntityMixin.java
@@ -1,22 +1,13 @@
 package coffee.waffle.emcutils.mixin;
 
 import coffee.waffle.emcutils.Caches;
-import com.google.common.collect.Lists;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.OtherClientPlayerEntity;
-import net.minecraft.client.network.PlayerListEntry;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @Mixin(PlayerEntity.class)
@@ -25,29 +16,6 @@ public abstract class PlayerEntityMixin {
 	@Inject(method = "getDisplayName", at = @At("HEAD"), cancellable = true)
 	private void emcutils$getDisplayName(CallbackInfoReturnable<Text> cir) {
 		PlayerEntity e = ((PlayerEntity) (Object) this);
-
-//		Collection<PlayerListEntry> playerList = MinecraftClient.getInstance().getNetworkHandler().getPlayerList();
-//
-//		PlayerListEntry entry = null;
-//		for (PlayerListEntry playerListEntry : playerList) {
-//			List<Text> siblings = playerListEntry.getDisplayName().getSiblings();
-//
-//			if (siblings.size() > 1 && siblings.get(1).contains(e.getName())) {
-//				entry = playerListEntry;
-//			}
-//		}
-//
-//		if (entry != null) {
-//			List<Text> siblings = Lists.newArrayList(entry.getDisplayName().getSiblings());
-//			siblings.remove(0); // Remove Server Tag
-//
-//			MutableText text = Text.empty();
-//			for (Text sibling : siblings) {
-//				text.append(sibling);
-//			}
-//
-//			cir.setReturnValue(text);
-//		}
 
 		try {
 			cir.setReturnValue(Caches.namePlateCache.get(e));
