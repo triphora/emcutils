ec804c53e3c92043ffd6e11ff0b7018313934752
Emma Alexia <emma@modrinth.com>
2025-12-21 16:11:20 -0500

    Update to 1.21.11

--- common/src/main/java/coffee/waffle/emcutils/EMCDataComponentTypes.java
+++ common/src/main/java/coffee/waffle/emcutils/EMCDataComponentTypes.java
@@ -1,7 +1,7 @@
 package coffee.waffle.emcutils;
 
 import coffee.waffle.emcutils.feature.UsableItems.UsableItem;
-import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
 import net.minecraft.component.ComponentType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -10,7 +10,7 @@ import java.util.function.UnaryOperator;
 
 public class EMCDataComponentTypes {
 	public static final ComponentType<UsableItem> USABLE_ITEM = register(
-		"usable_item", builder -> builder.codec(Codec.unit(UsableItem.ITEM)).cache()
+		"usable_item", builder -> builder.codec(MapCodec.unitCodec(UsableItem.ITEM)).cache()
 	);
 
 	private static <T> ComponentType<T> register(String id, UnaryOperator<ComponentType.Builder<T>> builderOperator) {
